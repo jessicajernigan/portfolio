a009a772a4d82f42d7c48b097c6e3516f91c1f84
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-10-06 18:50:17 -0500

    Updates

---
+++ precache-manifest.31645d0521dad211e805993d05c5157b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd85bea370e6b55fa2563843204789c6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a93446eb3cb8e9613dcd",
+    "url": "/portfolio/static/css/main.e4ec1a9b.chunk.css"
+  },
+  {
+    "revision": "b47b52950ba31b36720b",
+    "url": "/portfolio/static/js/2.2fca8df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.2fca8df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a93446eb3cb8e9613dcd",
+    "url": "/portfolio/static/js/main.e6fdaee4.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f5f6cd4c64b89e3b9582135367006dbe",
+    "url": "/portfolio/static/media/PasswordGenerator.f5f6cd4c.png"
+  },
+  {
+    "revision": "16f51a24e335da3fff40927b6f6c41f3",
+    "url": "/portfolio/static/media/WeatherDashBoard.16f51a24.png"
+  },
+  {
+    "revision": "7f09a875fa7a13613e975f2466dd74e6",
+    "url": "/portfolio/static/media/barkeep.7f09a875.png"
+  },
+  {
+    "revision": "54026ce52e59d1f00d41fd33014dab0f",
+    "url": "/portfolio/static/media/employeedb.54026ce5.png"
+  },
+  {
+    "revision": "3ca96da7d8a508c3272324100b4f2a24",
+    "url": "/portfolio/static/media/headshot.3ca96da7.png"
+  },
+  {
+    "revision": "8353b9123050c149b9d8b1abad70df52",
+    "url": "/portfolio/static/media/headshot2.8353b912.png"
+  },
+  {
+    "revision": "0944ec5cb946bfc3bb4b06368ca95b52",
+    "url": "/portfolio/static/media/meettheteam.0944ec5c.png"
+  },
+  {
+    "revision": "6183e7e4ca884514174d77975d0e61b0",
+    "url": "/portfolio/static/media/moroccan-flower.6183e7e4.png"
+  },
+  {
+    "revision": "4c8bd9969151ee2eb21f8cd8ab976845",
+    "url": "/portfolio/static/media/techblog.4c8bd996.png"
+  },
+  {
+    "revision": "78ee9c8a6a9f322010a59e932bc83920",
+    "url": "/portfolio/static/media/vintage-wallpaper.78ee9c8a.png"
+  }
+]);
